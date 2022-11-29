f2bae4f62f0a6d8ca9d1e21a81bbd48e3460e24f
luisesales <duduosales@hotmail.com>
2022-11-28 23:10:22 -0300

    linkando conteúdo já feito

---
+++ src/sudoku.h
@@ -0,0 +1,33 @@
+#ifndef _SUDOKU_H_
+#define _SUDOKU_H_
+
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+enum gameState{
+    GAMENTRANCE = 0,
+    GAMEINIT,
+    UNMAKEPLAY,
+    ASKADVIC,
+};
+namespace sudoku{
+
+    /// Leitor de configuração do jogo
+    class gameConfig{
+        
+    };
+
+    /// Controlador do jogo em andamento
+    class gameController{
+
+    };
+
+    /// Gerador dos resultados do controller
+    class gamePrinter{
+
+    };
+};
+
+#endif
